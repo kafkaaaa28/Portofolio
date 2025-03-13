1d4c4de147677e914a6c0058082b45f12750411f
kafkaaaa28 <kafkafj1@gmail.com>
2025-03-14 00:50:51 +0700

    tambahkan Dock

--- src/App.js
+++ src/App.js
@@ -2,13 +2,43 @@ import './App.css';
 import Hero from './Hero/Hero';
 import Navbars from './Navbars/Navbars';
 import About from './About/About';
+import Dock from './Dock/Dock';
 import { HashRouter as Router } from 'react-router-dom';
 import Scrollvelo from './Scroll/Scrollvelo';
 import Timeline from './Timeline/Timeline';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { IoHomeOutline } from 'react-icons/io5';
+import { FaRegFolderOpen } from 'react-icons/fa';
+import { VscAccount } from 'react-icons/vsc';
+import { CiPhone } from 'react-icons/ci';
 import ButtonSee from './Button/ButtonSee';
 function App() {
   const [Block, setBlock] = useState(false);
+  const [Mobile, setMobile] = useState(false);
+  const items = [
+    { icon: <IoHomeOutline size={18} />, label: 'Home', onClick: () => alert('Home!') },
+    { icon: <VscAccount size={18} />, label: 'Archive', onClick: () => alert('Archive!') },
+    { icon: <FaRegFolderOpen size={18} />, label: 'Profile', onClick: () => alert('Profile!') },
+    { icon: <CiPhone size={18} />, label: 'Settings', onClick: () => alert('Settings!') },
+  ];
+
+  useEffect(() => {
+    const checkMobile = () => {
+      if (window.innerWidth < 640) {
+        setMobile(true);
+      } else {
+        setMobile(false);
+      }
+    };
+
+    checkMobile();
+
+    window.addEventListener('resize', checkMobile);
+
+    return () => {
+      window.removeEventListener('resize', checkMobile);
+    };
+  }, []);
   const Toggler = () => {
     const Screen = document.getElementById('Screen');
     setBlock(!Block);
@@ -31,6 +61,15 @@ function App() {
             <Scrollvelo />
             <About />
             <Timeline />
+            {Mobile ? (
+              <>
+                <div className="Dock">
+                  <Dock items={items} panelHeight={68} baseItemSize={50} magnification={70} />
+                </div>
+              </>
+            ) : (
+              ''
+            )}
           </>
         ) : (
           ''
--- src/Button/ButtonSee.jsx
+++ src/Button/ButtonSee.jsx
@@ -1,13 +1,6 @@
 import './ButtonSee.css';
 
 const ButtonSee = () => {
-  return (
-    <button class="button" data-text="Awesome">
-      <span class="actual-text">&nbsp;kafka&nbsp;</span>
-      <span aria-hidden="true" class="hover-text">
-        &nbsp;Kafka&nbsp;
-      </span>
-    </button>
-  );
+  return <button className="buttons"> Click</button>;
 };
 export default ButtonSee;
